b183d0ef595889cc4420f2164633b2d136ca563d
Scott <dscamiss@gmail.com>
2024-12-18 11:01:21 -0800

    Minor update to fully_connected.py

---
+++ src/examples/fully_connected/__init__.py
@@ -0,0 +1,5 @@
+"""Examples module."""
+
+from examples.fully_connected.fully_connected import FullyConnected
+
+__all__ = ["FullyConnected"]
--- src/examples/fully_connected/fully_connected.py
+++ src/examples/fully_connected/fully_connected.py
@@ -6,13 +6,14 @@ from typeguard import typechecked as typechecker
 
 
 class FullyConnected(nn.Module):
-    """Fully-connected neural network.
+    """Fully-connected neural network with ReLU activations.
 
     Args:
         input_dim: Input dimension.
         hidden_layer_dims: Hidden layer dimensions.
         output_dim: Output dimension.
         negative_slope: Negative slope for leaky ReLU (default = 0.0).
+        final_activation: Apply activation at final layer (default = True)
     """
 
     def __init__(  # noqa: DCO010
@@ -21,19 +22,22 @@ class FullyConnected(nn.Module):
         hidden_layer_dims: list[int],
         output_dim: int,
         negative_slope: float = 0.0,
+        final_activation: bool = True,
     ):
         super().__init__()
         self.input_dim = input_dim
         self.hidden_layer_dims = hidden_layer_dims
         self.output_dim = output_dim
         self.negative_slope = negative_slope
+        self.final_activation = final_activation
 
         layers = []
 
         if not hidden_layer_dims:
             # Edge case: No hidden layers
             layers.append(nn.Linear(input_dim, output_dim))
-            layers.append(nn.LeakyReLU(negative_slope))
+            if final_activation:
+                layers.append(nn.LeakyReLU(negative_slope))
         else:
             # Generic case: At least one hidden layer
             layers = []
@@ -45,7 +49,8 @@ class FullyConnected(nn.Module):
                 layers.append(nn.LeakyReLU(negative_slope))
 
             layers.append(nn.Linear(hidden_layer_dims[-1], output_dim))
-            layers.append(nn.LeakyReLU(negative_slope))
+            if final_activation:
+                layers.append(nn.LeakyReLU(negative_slope))
 
         self.layers = nn.Sequential(*layers)
 
